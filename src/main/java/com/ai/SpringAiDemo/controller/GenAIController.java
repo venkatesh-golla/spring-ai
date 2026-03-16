2c14ce29aa74cc5c0154de1eac01b10a74a99757
venkatesh-golla <golla.venkatesh1997@gmail.com>
2026-03-15 17:14:47 -0700

    Add support for image generation using prompt

--- src/main/java/com/ai/SpringAiDemo/controller/GenAIController.java
+++ src/main/java/com/ai/SpringAiDemo/controller/GenAIController.java
@@ -1,15 +1,26 @@
 package com.ai.SpringAiDemo.controller;
 
 import com.ai.SpringAiDemo.service.ChatService;
+import com.ai.SpringAiDemo.service.ImageService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.List;
+
 @RestController
 public class GenAIController {
+    private final ChatService chatService;
+    private final ImageService imageService;
     @Autowired
-    private ChatService chatService;
+    public GenAIController(ChatService chatService, ImageService imageService) {
+        this.chatService = chatService;
+        this.imageService = imageService;
+    }
 
     @GetMapping("/chat")
     public String getResponse(@RequestParam String prompt){
@@ -19,5 +30,23 @@ public class GenAIController {
     @GetMapping("/chat-options")
     public String getResponseWithOptions(@RequestParam String prompt){
         return chatService.getResponseOptions(prompt);
+    }
+
+    @GetMapping("/image")
+    public void generateImage(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse iamgeResponse= imageService.generateImage(prompt);
+        String url = iamgeResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(url);
+    }
+
+    @GetMapping("/image-options")
+    public List<String> generateImageWithOptions(
+            @RequestParam String prompt,
+            @RequestParam (defaultValue = "hd") String quality,
+            @RequestParam (defaultValue = "1024") int height,
+            @RequestParam (defaultValue = "1024") int width,
+            @RequestParam (defaultValue = "1") int n) {
+        ImageResponse imageResponse= imageService.generateImageOptions(prompt,quality,height,width,n);
+        return imageResponse.getResults().stream().map(result -> result.getOutput().getUrl()).toList();
     }
 }
---
+++ src/main/java/com/ai/SpringAiDemo/service/ImageService.java
@@ -0,0 +1,36 @@
+package com.ai.SpringAiDemo.service;
+
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+//    Grok does not support Image generation, we would need to use OpenAI's API key in application.properties for this functionality
+    private final OpenAiImageModel imageModel;
+    @Autowired
+    public ImageService(OpenAiImageModel imageModel) {
+        this.imageModel = imageModel;
+    }
+
+    public ImageResponse generateImage(String prompt){
+        return imageModel.call(
+                new ImagePrompt(prompt)
+        );
+    }
+
+    public ImageResponse generateImageOptions(String prompt, String quality, int height, int width, int n){
+        return imageModel.call(
+                new ImagePrompt(prompt,
+                OpenAiImageOptions.builder()
+                        .withQuality(quality)
+                        .withHeight(height)
+                        .withWidth(width)
+                        .withN(n)
+                        .build()
+        ));
+    }
+}
